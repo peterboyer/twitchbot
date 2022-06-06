c2b0c8b74ffe7c264f2637fd4e206546c9885fe7
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-06-06 11:55:57 +1000

    feat: imap listener working

--- src/imap.ts
+++ src/imap.ts
@@ -1,13 +1,26 @@
 import Imap from "imap";
+
 import { getEnvOrThrow } from "./env";
 
-export async function listen() {
+interface Message {
+  id: number;
+  buffer: string;
+  headers: Record<string, string[]>;
+  attributes: Record<string, unknown>;
+}
+
+export async function listen(options: {
+  onMessage?: (message: Message) => void;
+  onMessages?: (messages: Message[]) => void;
+}) {
   const config = {
     user: getEnvOrThrow("IMAP_USER"),
     password: getEnvOrThrow("IMAP_PASSWORD"),
     host: getEnvOrThrow("IMAP_HOST"),
     port: parseInt(getEnvOrThrow("IMAP_PORT"), 10),
   };
+
+  const { onMessage, onMessages } = options;
 
   const client = new Imap({
     user: config.user,
@@ -22,14 +35,65 @@ export async function listen() {
 
   client.once("ready", () => {
     console.log("ready");
-    client.openBox("INBOX", false, (err, mailbox) => {
+    client.openBox("INBOX", false, (err) => {
       if (err) {
         console.error(err);
         return;
       }
-      console.log("connected", mailbox);
-      client.on("mail", console.log);
-      client.on("update", console.log);
+      console.log("connected");
+      const parseUnread = () => {
+        client.search(["UNSEEN"], (err, ids) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          if (!ids.length) {
+            return;
+          }
+          console.log("search", ids);
+          const $ = client.fetch(ids, { bodies: "" });
+          const messages = new Map<number, Message>();
+          const callback = (id: number, message: Message) => {
+            messages.set(id, message);
+            onMessage && onMessage(message);
+            if (messages.size === ids.length) {
+              onMessages && onMessages(Array.from(messages.values()));
+            }
+          };
+          $.on("message", (msg, id) => {
+            console.log(`message ${id}`);
+            let buffer = "";
+            let attributes: Record<string, string> = {};
+            msg.on("body", (stream) => {
+              stream.on("data", (chunk) => {
+                buffer += chunk.toString("utf8");
+              });
+            });
+            msg.once("attributes", (attrs) => {
+              attributes = attrs;
+            });
+            msg.once("end", () => {
+              const message: Message = {
+                id,
+                buffer,
+                headers: Imap.parseHeader(buffer),
+                attributes,
+              };
+              callback(id, message);
+            });
+          });
+        });
+      };
+      client.on("mail", (...args: unknown[]) => {
+        console.log("mail", ...args);
+        parseUnread();
+      });
+      client.on("update", (...args: unknown[]) => {
+        console.log("update", ...args);
+        parseUnread();
+      });
+      console.log("initial read");
+      parseUnread();
     });
   });
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import "dotenv/config";
-
 // import * as twitch from "./twitch";
 import * as imap from "./imap";
 
-imap.listen();
+imap.listen({
+  onMessages: (messages) => {
+    const hasDonations = messages.some(
+      (message) => message.headers["from"]?.[0] === "info@wegive.com.au"
+    );
+    if (!hasDonations) {
+      console.log("no matches...");
+      return;
+    }
+    for (const message of messages) {
+      console.log(
+        `message: ${message.headers["from"]?.[0]} "${message.headers["subject"]?.[0]}"`
+      );
+    }
+  },
+});
