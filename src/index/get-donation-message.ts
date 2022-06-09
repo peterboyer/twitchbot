edd89f9776190d8c74c99f28c6036f2985366acd
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-06-09 10:29:47 +1000

    fix: update chat message

--- src/index/get-donation-message.ts
+++ src/index/get-donation-message.ts
@@ -5,10 +5,11 @@ export function getDonationMessage(donation: Donation) {
   const { name, amount, username } = donation;
   const displayName = getDisplayName({ name, username });
   const message = [
-    "DONATION!",
-    `${displayName} just donated ${amount} for the Cancer Council!`,
+    "<3 DONATION! <3",
+    `${displayName} just donated ${amount} towards the fight against cancer!`,
     "F**K CANCER!",
-    "I appreciate each and every donation; type !donate in chat for link and instructions.",
+    "Thank you so much! I appreciate each and every donation.",
+    "Type !fightcancer in chat for donation link and instructions.",
   ].join(" ");
   return message;
 }
